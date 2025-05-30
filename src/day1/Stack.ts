ed652747e3433ffebb4198c50a309f7ef7277363
Francisco Andrade <frma059@gmail.com>
2025-05-30 15:07:14 -0300

    stack with singly linked list

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,22 @@
-export default class Stack<T> {
-    public length: number;
+import SinglyLinkedList from "./SinglyLinkedList";
 
-    
+export default class Stack<T> {
+    public list: SinglyLinkedList<T> = new SinglyLinkedList<T>();
+    public length: number = this.list.length;
 
     constructor() {
     }
 
     push(item: T): void {
-
-}
+        this.list.append(item);
+        this.length = this.list.length;
+    }
     pop(): T | undefined {
-
-}
+        const result = this.list.removeAt(this.list.length - 1);
+        this.length = this.list.length;
+        return result;
+    }
     peek(): T | undefined {
-
-}
+        return this.list.get(this.list.length - 1);
+    }
 }
