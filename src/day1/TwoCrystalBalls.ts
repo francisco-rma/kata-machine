6c742c19505eabcfeeb4209ed27f4a4d287af5a2
Francisco Andrade <frma059@gmail.com>
2025-05-30 10:25:32 -0300

    two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,24 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let left: number = 0;
+    let right: number = breaks.length - 1;
+    let mid: number;
 
+    while (left <= right) {
+        mid = Math.floor(left + ((right - left) / 2));
+
+        if (mid < breaks.length - 1 && breaks[mid + 1] !== breaks[mid]) {
+            return mid + 1;
+        }
+        else if (mid > 0 && breaks[mid - 1] !== breaks[mid]) {
+            return mid;
+        }
+        else if (breaks[mid] === false) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
 }
