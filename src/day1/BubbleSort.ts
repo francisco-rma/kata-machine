9bf89f9815cf46b4591ab01beb23388fefac4c97
Francisco Andrade <frma059@gmail.com>
2025-05-30 11:03:50 -0300

    bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,13 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let limit = arr.length - 1; limit > 0; limit--) {
+        let i: number = 0;
+        while (i < limit) {
+            if (arr[i] > arr[i + 1]) {
+                const aux = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = aux;
+            }
+            i += 1;
+        }
+    }
 }
