f34e16c5e8e728286f68b2d8d92edd3f527caaf3
Francisco Andrade <frma059@gmail.com>
2025-05-30 14:34:33 -0300

    queue with singly linked list

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,22 @@
-export default class Queue<T> {
-    public length: number;
+import SinglyLinkedList from "./SinglyLinkedList";
 
-    
+export default class Queue<T> {
+    public list: SinglyLinkedList<T> = new SinglyLinkedList<T>();
+    public length: number = this.list.length;
 
     constructor() {
     }
 
     enqueue(item: T): void {
-
-}
+        this.list.prepend(item);
+        this.length = this.list.length;
+    }
     deque(): T | undefined {
-
-}
+        const result = this.list.removeAt(this.list.length - 1);
+        this.length = this.list.length;
+        return result;
+    }
     peek(): T | undefined {
-
-}
+        return this.list.get(this.list.length - 1);
+    }
 }
