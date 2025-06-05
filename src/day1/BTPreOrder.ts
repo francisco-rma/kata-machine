cb019072c7f5516a4ff9852e30797d41ea8aa845
Francisco Andrade <frma059@gmail.com>
2025-06-05 15:37:33 -0300

    depth first traversal

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,24 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function dfs(node: BinaryNode<number>, out: number[]): number[] {
+    if (!(node.left || node.right)) {
+        console.log("Leaf node: ", node);
+        out.push(node.value);
+        return out;
+    }
 
+    out.push(node.value);
+    if (node.left) {
+        out = dfs(node.left, out);
+    }
+    if (node.right) {
+        out = dfs(node.right, out);
+    }
+
+    console.log("result: ", out);
+    return out;
+
+}
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    let out: number[] = [];
+    const result = dfs(head, out);
+    return result;
 }
