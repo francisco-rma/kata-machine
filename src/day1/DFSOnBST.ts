056af52ad294fb5e4f82408c7480fc13d5d5c1c7
Francisco Andrade <frma059@gmail.com>
2025-06-05 20:41:50 -0300

    dfs on bst

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,18 @@
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    let node: BinaryNode<number> | null = head;
+    let result: boolean = false;
+    while (node) {
+        if (node.value === needle) {
+            result = true;
+            break;
+        }
+        else if (node.value < needle) {
+            node = node.right;
+        }
+        else {
+            node = node.left;
+        }
+    }
 
+    return result;
 }
