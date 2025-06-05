18660ba752bb9fd6931c2f3882a453a8306b5a53
Francisco Andrade <frma059@gmail.com>
2025-06-05 16:05:14 -0300

    bt in order traversal

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,26 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function dfs(node: BinaryNode<number>, out: number[]): number[] {
+    if (!(node.left || node.right)) {
+        console.log("Leaf node: ", node);
+        out.push(node.value);
+        return out;
+    }
 
+    if (node.left) {
+        out = dfs(node.left, out);
+    }
+    
+    out.push(node.value);
+
+    if (node.right) {
+        out = dfs(node.right, out);
+    }
+
+    console.log("result: ", out);
+    return out;
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    let out: number[] = [];
+    const result = dfs(head, out);
+    return result;
 }
