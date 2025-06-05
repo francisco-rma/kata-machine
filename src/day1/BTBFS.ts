e244d05040fbbe0626dbe5aaacdd6d6d660abc53
Francisco Andrade <frma059@gmail.com>
2025-06-05 16:21:06 -0300

    binary tree breadth first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,28 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+import Queue from "./Queue";
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    let queue: Queue<BinaryNode<number>> = new Queue<BinaryNode<number>>();
+    let visited: Set<BinaryNode<number>> = new Set<BinaryNode<number>>();
+
+    queue.enqueue(head);
+    let result: boolean = false;
+
+    while (queue) {
+        let node: BinaryNode<number> | undefined = queue.deque();
+        if (!node) {
+            break;
+        }
+        if (node.value === needle) {
+            result = true;
+            break;
+        }
+        if (node.left) {
+            queue.enqueue(node.left);
+        }
+        if (node.right) {
+            queue.enqueue(node.right);
+        }
+    }
+
+    return result;
 }
