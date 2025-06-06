88491d465f47c0d6414852f49d327075a23908ac
Francisco Andrade <frma059@gmail.com>
2025-06-06 12:24:40 -0300

    WIP binary tree insertion

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,5 +1,6 @@
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
     let node: BinaryNode<number> | null = head;
+    let parent: BinaryNode<number> | null = head;
     let result: boolean = false;
     while (node) {
         if (node.value === needle) {
@@ -15,4 +16,46 @@ export default function dfs(head: BinaryNode<number>, needle: number): boolean {
     }
 
     return result;
+}
+
+export function df_parent_search(head: BinaryNode<number>, needle: number): BinaryNode<number> {
+    let node: BinaryNode<number> | null = head;
+    let parent: BinaryNode<number> | null = null;
+
+    while (node) {
+        if (node.value === needle) {
+            break;
+        }
+        else if (needle > node.value) {
+            parent = node;
+            node = node.right;
+        }
+        else {
+            parent = node;
+            node = node.left;
+        }
+    }
+    if (!parent) {
+        return head;
+    }
+    return parent;
+}
+
+export function dfs_insert(head: BinaryNode<number>, new_item: number): [BinaryNode<number> | null, BinaryNode<number> | null] {
+    let parent: BinaryNode<number> = df_parent_search(head, new_item);
+    let node: BinaryNode<number> = { value: new_item } as BinaryNode<number>;
+
+    if (parent.left && parent.right) {
+        throw new Error("Node is full");
+    }
+    else {
+        if (node.value <= parent.value && !parent.left) {
+            parent.left = node;
+        }
+        if (node.value > parent.value && !parent.right) {
+            parent.right = node;
+        }
+    }
+
+    return [node, parent];
 }
